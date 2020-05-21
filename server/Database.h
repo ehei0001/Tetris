79a5bd8864a41818a51df919fe970a8d6181e9c8
ehei <ehei@naver.com>
2020-05-21 21:43:19 +0900

    some instance change to template parameter

--- server/Database.h
+++ server/Database.h
@@ -24,22 +24,22 @@ public:
 		std::string m_sql;
 
 	public:
-		template<class... Ts>
+		template<size_t COLUMN_COUNT, class T, class... Ts>
 		class Iterator
 		{
-			const int m_column_count = sizeof...(Ts);
-			using ValueType = std::variant<Ts...>;
+			using ValueType = std::variant<T, Ts...>;
 			using RecordType = std::vector<ValueType>;
 
 			sqlite3_stmt* m_statement{};
 			int m_status{ SQLITE_DONE };
-			int m_column_index{ -1 };
 
 			const size_t m_hash{};
 
 		public:
 			explicit Iterator(std::string sql, sqlite3* db, size_t hash) : m_status{ ::sqlite3_prepare_v2(db, sql.c_str(), -1, &m_statement, {}) }, m_hash{ hash }
 			{
+				assert(::sqlite3_column_count(m_statement) == COLUMN_COUNT);
+
 				if (m_status == SQLITE_OK) {
 					m_status = ::sqlite3_step(m_statement);
 				} else {
@@ -81,7 +81,10 @@ public:
 			RecordType operator*()
 			{
 				if (m_status == SQLITE_ROW) {
-					return { _get_column<Ts>(m_statement)... };
+					RecordType record;
+					_get_columns<T, Ts...>(m_statement, record);
+					
+					return record;
 				}
 				else {
 					assert(m_status == SQLITE_DONE);
@@ -102,8 +105,22 @@ public:
 			}
 
 		private:
+			template<class T, class...Ts>
+			void _get_columns(sqlite3_stmt* statement, RecordType& record)
+			{
+				constexpr size_t column_index = COLUMN_COUNT - sizeof...(Ts) - 1;
+				static_assert(column_index < COLUMN_COUNT, "invalid column index");
+
+				auto value{ _get_column<T>(statement, column_index) };
+				record.emplace_back(std::move(value));
+
+				if constexpr (sizeof...(Ts) > 0) {
+					_get_columns<Ts...>(statement, record);
+				}
+			}
+
 			template<class T>
-			ValueType _get_column(sqlite3_stmt* statement)
+			ValueType _get_column(sqlite3_stmt* statement, size_t column_index)
 			{
 				static_assert(false, "not implemented type");
 
@@ -111,30 +128,22 @@ public:
 			}
 
 			template<>
-			ValueType _get_column<int>(sqlite3_stmt* statement)
+			ValueType _get_column<int>(sqlite3_stmt* statement, size_t column_index)
 			{
-				auto column = _get_column_index();
-
-				return ::sqlite3_column_int(m_statement, column);
+				return ::sqlite3_column_int(m_statement, column_index);
 			}
 
 			template<>
-			ValueType _get_column<std::string>(sqlite3_stmt* statement)
+			ValueType _get_column<std::string>(sqlite3_stmt* statement, size_t column_index)
 			{
-				auto column = _get_column_index();
-				auto text{ ::sqlite3_column_text(m_statement, column) };
+				auto text{ ::sqlite3_column_text(m_statement, column_index) };
 				auto text_{ reinterpret_cast<const char*>(text) };
 
 				return std::string{ text_ };
 			}
 
-			int _get_column_index()
-			{
-				return ++m_column_index % m_column_count;
-			}
-
 			template<class T>
-			size_t _get_hash(T t)
+			size_t _get_hash(T t) const
 			{
 				return std::hash<T>{}(t);
 			}
@@ -148,14 +157,14 @@ public:
 		{
 			static_assert(sizeof(this) == sizeof(size_t), "invalid size");
 
-			return Iterator<Ts...>{ m_sql, m_db, reinterpret_cast<size_t>(this) };
+			return Iterator<sizeof...(Ts), Ts...>{ m_sql, m_db, reinterpret_cast<size_t>(this) };
 		}
 
 		auto end()
 		{
 			static_assert(sizeof(this) == sizeof(size_t), "invalid size");
 
-			return Iterator<Ts...>{ m_sql, reinterpret_cast<size_t>(this) };
+			return Iterator<sizeof...(Ts), Ts...>{ m_sql, reinterpret_cast<size_t>(this) };
 		}
 	};
 
